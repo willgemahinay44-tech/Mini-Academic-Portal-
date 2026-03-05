2e54a9b5d44355aabc076e8aa639d91648265f25
Willge-Mahinay <willgemahinay44@gmail.com>
2026-03-05 18:54:04 +0800

    Santum API authentication

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,19 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/token-test', function () {
-    $user = User::query()->firstOrFail();
+    $user = User::query()->first();
+
+    if (! $user) {
+        $user = User::query()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
 
     return $user->createToken('test')->plainTextToken;
 });
